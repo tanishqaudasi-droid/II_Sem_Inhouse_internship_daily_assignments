31053e0a1ad4af4578be6fee09860d4a1356c8f7
tanishqaudasi-droid <tanishqaudasi@gmail.com>
2026-07-11 10:01:07 +0530

    updated

---
+++ day7/index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello world";
+$student = ["mohit", "rohit", "Ram"];
+print_r($student);
+$student_marks = [12, 34, 45];
+print_r($student_marks);
+$students = [
+    "mohit" => 12,
+    "rohit" => 34
+    
+];
+print_r($students["Rohan"]);
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+
+
+?>
